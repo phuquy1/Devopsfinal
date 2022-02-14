b6c0ce515dfe90e34e66ccb41eeec0140e3a90a8
Le Dat <ledat@Les-MacBook-Pro.local>
2022-02-14 17:21:14 +0700

    Update passport.js

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -48,20 +48,23 @@ passport.use(
       passReqToCallback: true,
     },
     async (req, username, password, done) => {
-      const { fullname } = req.body;
+      try{
+        const { fullname } = req.body;
+        let newUser = {
+          fullname,
+          username,
+          password,
+        };
 
-      let newUser = {
-        fullname,
-        username,
-        password,
-      };
+        newUser.password = await helpers.encryptPassword(password);
 
-      newUser.password = await helpers.encryptPassword(password);
-
-      // Saving in the Database
-      const [result] = await pool.query("INSERT INTO users SET ? ", newUser);
-      newUser.id = result.insertId;
-      return done(null, newUser);
+        // Saving in the Database
+        const [result] = await pool.query("INSERT INTO users SET ? ", newUser);
+        newUser.id = result.insertId;
+        return done(null, newUser);
+      }catch(e){
+        console.log(e);
+      }
     }
   )
 );
